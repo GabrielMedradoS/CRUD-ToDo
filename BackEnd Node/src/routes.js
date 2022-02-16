f3607d267add309669bd41882e5b90cd1aa9bc04
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-02-15 22:00:40 -0300

    Update To do

--- BackEnd Node/src/routes.js
+++ BackEnd Node/src/routes.js
@@ -1,23 +1,50 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 
-const allToDos = [{ nome: "aaa", status: false }];
+const allToDos = [{ name: "aaa", status: false }];
 
 //C
-router.post("/todos", (request, response) => {
+router.post("/todos", async (request, response) => {
   const { name } = request.body;
+  const createToDo = await prisma.todo.create({
+    data: {
+      name,
+    },
+  });
 
-  allToDos.push({ name, status: false });
-  return response.status(201).json(allToDos);
+  return response.status(201).json(createToDo);
 });
 
 //R
 router.get("/todos", (request, response) => {
-  return response.status(200).json(allToDos);
+  const getAllToDos = await prisma.todo.findMany();
+  return response.status(200).json(getAllToDos);
 });
 
 //U
+router.put("/todos", async (request, response) => {
+  const { name, id, status } = request.body;
+
+  if (!id) {
+    return response.status(400).json("Id is mandatory");
+  }
+
+  const updateToDo = await prisma.todo.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      status,
+    },
+  });
+
+  return response.status(200).json(updateToDo);
+});
+
 //D
 
 export { router };
