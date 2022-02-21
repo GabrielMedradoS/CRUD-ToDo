58f135ad2c70473d034e8b504e84ebcc08e3dcb8
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-02-21 11:30:56 -0300

    Header

--- FrontEnd Vite/src/App.jsx
+++ FrontEnd Vite/src/App.jsx
@@ -1,45 +1,48 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
+
+const arrayTodos = [
+  { name: "Limpar a casa", status: true },
+  { name: "Limpar o cachorro", status: false },
+];
+
+const Todos = ({ todos }) => {
+  return (
+    <div className="todosDiv">
+      {todos.map((todo) => {
+        return (
+          <div className="todo">
+            <button
+              className="checkbox"
+              style={{ backgroundColor: todo.status ? "#A879E6" : "white" }}
+            ></button>
+            <p>{todo.name}</p>
+            <button>
+              <AiOutlineEdit size={20} color={"#64697b"} />
+            </button>
+            <button>
+              <AiOutlineDelete size={20} color={"#64697b"} />
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+      <header className="container">
+        <div className="header">
+          <h1>Dont be lazy</h1>
+        </div>
+        <div className="todosDiv">
+          <Todos todos={arrayTodos}></Todos>
+        </div>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
