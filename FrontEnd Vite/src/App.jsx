61c0a4f9de299249719736db0ff32c1de25dd360
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2022-02-24 11:50:41 -0300

    Delete Fucntion

--- FrontEnd Vite/src/App.jsx
+++ FrontEnd Vite/src/App.jsx
@@ -4,34 +4,55 @@ import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const Todos = ({ todos }) => {
-  return (
-    <div className="todosDiv">
-      {todos.map((todo) => {
-        return (
-          <div className="todo">
-            <button
-              className="checkbox"
-              style={{ backgroundColor: todo.status ? "#A879E6" : "white" }}
-            ></button>
-            <p>{todo.name}</p>
-            <button>
-              <AiOutlineEdit size={20} color={"#64697b"} />
-            </button>
-            <button>
-              <AiOutlineDelete size={20} color={"#64697b"} />
-            </button>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
-
 function App() {
+  const Todos = ({ todos }) => {
+    return (
+      <div className="todosDiv">
+        {todos.map((todo) => {
+          return (
+            <div className="todo">
+              <button
+                className="checkbox"
+                style={{ backgroundColor: todo.status ? "#A879E6" : "white" }}
+              ></button>
+              <p>{todo.name}</p>
+              <button>
+                <AiOutlineEdit size={20} color={"#64697b"} />
+              </button>
+              <button>
+                <AiOutlineDelete
+                  onClick={() => deleteTodo(todo)}
+                  size={20}
+                  color={"#64697b"}
+                />
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  async function handleWitthNewButton() {
+    setInputVisibility(!inputVisibility);
+  }
+
   async function getTodos() {
     const response = await axios.get("http://localhost:3333/todos");
     setTodos(response.data);
+  }
+
+  async function createTodo() {
+    const response = await axios.post("http://localhost:3333/todos", {
+      name: inputValue,
+    });
+    getTodos();
+    setInputVisibility(!inputVisibility);
+  }
+
+  async function deleteTodo(todo) {
+    await axios.delete(`http://localhost:3333/todos/${todo.id}`);
+    getTodos();
   }
 
   const [todos, setTodos] = useState([]);
@@ -58,7 +79,12 @@ function App() {
           type="text"
           className="inputName"
         />
-        <button className="newTaskButton">+ New Task</button>
+        <button
+          onClick={inputVisibility ? createTodo : handleWitthNewButton}
+          className="newTaskButton"
+        >
+          {inputVisibility ? "Confirm" : "+ New Task"}
+        </button>
       </header>
     </div>
   );
